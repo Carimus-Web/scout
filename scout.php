9c48051c42cd2323dcfdb15888e101fb3e395e96
Skylar Wuebker <skylar.wuebker@carimus.com>
2026-05-22 12:47:42 -0400

    fixes typo

--- scout.php
+++ scout.php
@@ -5,7 +5,7 @@
  * Version: 1.0.13
  * Author: Carimus
  * Author URI: https://carimus.com
- * Plugin URI: https://github.com/carimus/scout
+ * Plugin URI: https://github.com/Carimus-Web/scout
  * License: GPL-2.0-or-later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: scout
